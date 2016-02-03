0802ecb6335d8f6d5bca2e0cf0626aab971dddaa
Stavros Kounis <skounis@gmail.com>
2016-02-03 10:22:12 +0200

    restaurant minified

--- stage/restaurant/scripts/configuration.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- angular.module('config', [])
-
-.constant('ENV', {name:'development',apiEndpoint:'http://dev.yoursite.com:10000/',youtubeKey:'AIzaSyDael5MmCQa1GKQNKQYypmBeB08GATgSEo',ionicPrivateKey:'a9265eaf15a20cc8516c770e8748aeed4891b28f453ce755',ionicPublicKey:'e30d4d540b8c75d1f167bbf242423c3fb23fe10275d1c016',ionicAppId:'241b6d37',gcmId:'228071472080',dataProvider:'LOCAL'})
-
-;
